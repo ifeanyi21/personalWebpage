839bcb028b58e72205a478a65ce57664292f1608
ifeanyi21 <85758228+ifeanyi21@users.noreply.github.com>
2021-07-06 09:31:53 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+$("#chk").click(function(){
+$("section").toggleClass("sectionDarkMode");
+$("h2,h3,i").toggleClass("headerDarkMode");
+$("p").toggleClass("paraDarkMode");
+ $("h4").toggleClass("h4DarkMode");
+$(".ifeanyi-span").toggleClass("darkModeifeanyi-span");
+$(".label").toggleClass("labelDarkMode");
+$("#modeIconSun").toggleClass("iconDarkMode");
+$("#modeIconMoon").toggleClass("iconDarkMode");
+});
