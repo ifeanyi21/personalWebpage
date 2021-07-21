0165590e60fc5c7fa785c8a5248eec61794c9d2f
ifeanyi21 <85758228+ifeanyi21@users.noreply.github.com>
2021-07-22 00:12:05 +0100

    Update index.js
    Localstorage

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+let button = document.getElementById("chk");
 $(document).scroll(function(){
     if($(document).scrollTop()>300){
         $(".myBtn").css("display", "inline-block");
@@ -6,14 +7,49 @@ $(document).scroll(function(){
         $(".myBtn").css("display", "none");
     }
 });
-
-$("#chk").click(function(){
-$("section").toggleClass("sectionDarkMode");
-$("h2,h3,i").toggleClass("headerDarkMode");
-$("p").toggleClass("paraDarkMode");
- $("h4").toggleClass("h4DarkMode");
-$(".ifeanyi-span").toggleClass("darkModeifeanyi-span");
-$(".label").toggleClass("labelDarkMode");
-$("#modeIconSun").toggleClass("iconDarkMode");
-$("#modeIconMoon").toggleClass("iconDarkMode");
-});
+function darkMode(){
+    $("section").addClass("sectionDarkMode");
+    $("h2,h3,i").addClass("headerDarkMode");
+    $("p").addClass("paraDarkMode");
+    $("h4").addClass("h4DarkMode");
+    $(".ifeanyi-span").addClass("darkModeifeanyi-span");
+    $(".label").addClass("labelDarkMode");
+    $("#modeIconSun").addClass("iconDarkMode");
+    $("#modeIconMoon").addClass("iconDarkMode");
+    button.setAttribute("checked","true");
+    localStorage.setItem("Mode","Dark-Mode");
+}
+function lightMode(){
+    $("section").removeClass("sectionDarkMode");
+    $("h2,h3,i").removeClass("headerDarkMode");
+    $("p").removeClass("paraDarkMode");
+    $("h4").removeClass("h4DarkMode");
+    $(".ifeanyi-span").removeClass("darkModeifeanyi-span");
+    $(".label").removeClass("labelDarkMode");
+    $("#modeIconSun").removeClass("iconDarkMode");
+    $("#modeIconMoon").removeClass("iconDarkMode");
+    button.removeAttribute("checked");
+    localStorage.setItem("Mode","Light-Mode");
+}
+function body (){
+    if(localStorage.getItem("Mode")===null){
+        localStorage.setItem("Mode","Light-Mode")
+    }
+    else if(localStorage.getItem("Mode")==="Light-Mode"){
+        lightMode();
+    }
+    else if(localStorage.getItem("Mode")==="Dark-Mode"){
+        darkMode();
+    }
+    
+}
+button.addEventListener("click",()=>{
+    if(localStorage.getItem("Mode")==="Light-Mode"){
+        darkMode();
+        
+        
+    }
+    else if(localStorage.getItem("Mode")==="Dark-Mode"){
+    lightMode();
+    }
+})
