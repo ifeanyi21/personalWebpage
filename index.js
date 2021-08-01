be2d368b3fbd8254f2c90485f50ed23bf4aa5116
ifeanyi21 <85758228+ifeanyi21@users.noreply.github.com>
2021-08-01 14:42:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,38 @@
 let button = document.getElementById("chk");
+$("#sendBtn").click((e) => {
+  e.preventDefault();
+  let name = document.querySelector(".Name").value;
+  let phone = document.querySelector(".Phone").value;
+  let email = document.querySelector(".Email").value;
+  let message = document.querySelector(".Message").value;
+
+  if (name === "" || phone === "" || email === "" || message === "") {
+    console.log("Fill all Fields");
+  } else {
+    const messageDetails = {
+      Name: name,
+      Phone: phone,
+      Email: email,
+      Message: message,
+    };
+    fetch("https://messages-5153a-default-rtdb.firebaseio.com/messages.json", {
+      method: "POST",
+      body: JSON.stringify(messageDetails),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    let clearName = document.querySelector(".Name");
+    clearName.value = "";
+    let clearPhone = document.querySelector(".Phone");
+    clearPhone.value = "";
+    let clearEmail = document.querySelector(".Email");
+    clearEmail.value = "";
+    let clearMessage = document.querySelector(".Message");
+    clearMessage.value = "";
+    setTimeout(() => alert("Message Sent!"), 1000);
+  }
+});
 $(document).scroll(function(){
     if($(document).scrollTop()>300){
         $(".myBtn").css("display", "inline-block");
